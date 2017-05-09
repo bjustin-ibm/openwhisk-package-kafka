a51819460a6ee7755d0f05e8dcbe86bcbb101320
Justin Berstler <bjustin@us.ibm.com>
2017-05-09 10:10:02 -0400

    Properly handle JSON parameter values
    In the case that parameter values are valid JSON strings, they come into the action as type dict. In this case, automatically convert the value back to a JSON string.

--- action/kafkaProduce.py
+++ action/kafkaProduce.py
@@ -1,5 +1,7 @@
-import ssl
 import base64
+import json
+import ssl
+
 from kafka import KafkaProducer
 from kafka.errors import NoBrokersAvailable
 
@@ -22,9 +24,9 @@ def main(params):
         # only use the key parameter if it is present
         if 'key' in validatedParams:
             messageKey = validatedParams['key']
-            producer.send(validatedParams['topic'], bytes(validatedParams['value'], 'utf-8'), key=bytes(messageKey, 'utf-8'))
+            producer.send(validatedParams['topic'], str(validatedParams['value']).encode('utf-8'), key=str(messageKey).encode('utf-8'))
         else:
-            producer.send(validatedParams['topic'], bytes(validatedParams['value'], 'utf-8'))
+            producer.send(validatedParams['topic'], str(validatedParams['value']).encode('utf-8'))
 
         producer.flush()
 
@@ -49,9 +51,19 @@ def validateParams(params):
     if len(missingParams) > 0:
         return (False, "You must supply all of the following parameters: {}".format(', '.join(missingParams)))
 
+    # When parameter values are valid JSON strings, they come in as type dict.
+    # If this is the case, convert the parameter value into a JSON string.
+    if type(params['value']) is dict:
+        validatedParams['value'] = json.dumps(params['value'])
+        print("Automatically converting value parameter to JSON string: {}".format(validatedParams['value']))
+
+    if 'key' in params and type(params['key']) is dict:
+        validatedParams['key'] = json.dumps(params['key'])
+        print("Automatically converting key parameter to JSON string: {}".format(validatedParams['key']))
+
     if 'base64DecodeValue' in params and params['base64DecodeValue'] == True:
         try:
-            validatedParams['value'] = base64.b64decode(params['value']).decode('utf-8')
+            validatedParams['value'] = base64.b64decode(validatedParams['value']).decode('utf-8')
         except:
             return (False, "value parameter is not Base64 encoded")
 
@@ -60,7 +72,7 @@ def validateParams(params):
 
     if 'base64DecodeKey' in params and params['base64DecodeKey'] == True:
         try:
-            validatedParams['key'] = base64.b64decode(params['key']).decode('utf-8')
+            validatedParams['key'] = base64.b64decode(validatedParams['key']).decode('utf-8')
         except:
             return (False, "key parameter is not Base64 encoded")
 
